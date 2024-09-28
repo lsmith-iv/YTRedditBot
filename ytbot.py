c5470949164c6e862afeb63eb68f5ec1cb554491
lsmith-iv <lylesmith4@gmail.com>
2024-09-28 16:59:53 -0400

    First commit. Able to scrape a specified subreddit for top post text.

---
+++ ytbot.py
@@ -0,0 +1,26 @@
+# YTBot - Create bot to collate posts on Reddit, generate a YT short, and upload to Youtube
+# inspired heavily by https://github.com/Shifty-The-Dev/RedditVideoGenerator
+import praw
+import sys
+
+# Use Praw (Python Reddit API Wrapper) to retrieve data from Reddit
+reddit = praw.Reddit(
+        client_id="4Vcc_3zN9Rj9TuoNwG4Geg",
+        client_secret="hNWD0GOjIVTMHZ2zGRoa5f_VuV2V7A",
+        user_agent="hacker_man1337",
+        username="hacker_man1337",
+        password="Gr5mn963!",
+)
+
+subreddit = sys.argv[1] #grab command-line arg
+subreddit = reddit.subreddit(subreddit)
+
+for submission in subreddit.top(time_filter="day", limit=1):
+    print(submission.title, submission.selftext)
+
+# Generate speech using TTS
+# TODO: implement using pyttsx3
+
+# Use MoviePy to edit together video
+
+# Store final mpeg for later uploading
